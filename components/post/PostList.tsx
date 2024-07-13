78abd2717e148f1e1171fd11998e9c50d0d67a68
minkyung <mxkxx@naver.com>
2024-07-13 23:08:01 +0900

    style: 일반 게시물 컴포넌트 레이아웃 수정

--- components/post/PostList.tsx
+++ components/post/PostList.tsx
@@ -1,5 +1,6 @@
 import useArticles from '@/lib/useArticles';
 import formatDate from '@/utils/formatDate';
+import classNames from 'classnames';
 import Image from 'next/image';
 import styles from './Post.module.scss';
 import PostImage from './PostImage';
@@ -8,27 +9,39 @@ function Post({ article }: TArticle) {
   const { title, content, image, writer, likeCount, createdAt } = article;
   const { nickname } = writer;
 
+  const detailClasses = classNames(styles.detail, {
+    [styles.withImage]: !!image,
+    [styles.withoutImage]: !image,
+  });
+
   return (
     <div className={styles.post}>
       <div className={styles.content}>
-        <div className={styles.detail}>
+        <div className={detailClasses}>
           <p className={styles.description}>{content}</p>
           {image && <PostImage image={image} title={title} />}
         </div>
         <div className={styles.metadata}>
           <div className={styles.info}>
+            <Image
+              src='images/ic_profile.svg'
+              width={24}
+              height={24}
+              alt='profile'
+            />
+
             <p className={styles.nickname}>{nickname}</p>
-            <div className={styles.like}>
-              <Image
-                src='images/ic_heart.svg'
-                width={16}
-                height={16}
-                alt='heart'
-              />
-              <p>{likeCount}</p>
-            </div>
+            <p className={styles.date}>{formatDate(createdAt)}</p>
           </div>
-          <p className={styles.date}>{formatDate(createdAt)}</p>
+          <div className={styles.like}>
+            <Image
+              src='images/ic_heart.svg'
+              width={16}
+              height={16}
+              alt='heart'
+            />
+            <p>{likeCount}</p>
+          </div>
         </div>
       </div>
     </div>

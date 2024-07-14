01135474124b7307f06dd229af9b626d1c9796b0
장아영 <jang-ayoung@jang-ayeong-ui-iMac.local>
2024-07-14 12:29:39 +0900

    Design: input css 변경

--- pages/boards/components/Posts/Input/index.tsx
+++ pages/boards/components/Posts/Input/index.tsx
@@ -2,19 +2,26 @@ import React from "react";
 import styles from "./styles.module.scss";
 import searchIcon from "@/assets/icons/ic_search.svg";
 import Image from "next/image";
+import { useState } from "react";
 interface InputProps {
   placeholder?: string;
 }
 
 export default function Input() {
+  const [inputValue, setInputValue] = useState("");
   return (
     <div className={styles["input-box"]}>
-      <Image className={styles['search-icon']} src={searchIcon} alt="돋보기아이콘" />
+      <Image
+        className={styles["search-icon"]}
+        src={searchIcon}
+        alt="돋보기아이콘"
+      />
       <input
         className={styles["input"]}
-        type="text"
+        type="search"
         name="searchItems"
         placeholder="검색할 상품을 입력해주세요"
+        onChange={(e) => setInputValue(e.target.value)}
       />
     </div>
   );

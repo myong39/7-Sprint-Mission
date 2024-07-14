6bfb3e2ae64a37696d0f47f7b8ddaeb8bdc3f5e3
clyde-yoon <a1056719@gmail.com>
2024-07-14 23:36:45 +0900

    💄 반응형 구현x 스타일링 뼈대작성

--- components/Article/BestArticle.tsx
+++ components/Article/BestArticle.tsx
@@ -3,21 +3,27 @@ import { Article } from '@/types/Article';
 import style from './BestArticle.module.css';
 import formatDate from '@/lib/formatDate';
 import Image from 'next/image';
+import bestMark from '@/public/images/FreeBoard/bestMark.png';
 
 const BestArticle: React.FC<Article> = ({ ...article }) => {
   const { title, image, likeCount, createdAt, writer } = article;
   return (
-    <div className={style.BestArticle}>
-      <div>
-        <h2>{title}</h2>
-        <div className={style.ImageContainer}>
-          {image && <Image fill src={image} alt="게시글 이미지 미리보기" />}
+    <div className={style.OutContainer}>
+      <div className={style.BestArticle}>
+        <Image src={bestMark} alt="Best Mark" className={style.BestMark} />
+        <div className={style.TitleAndImage}>
+          <h2 className={style.Title}>{title}</h2>
+          <div className={style.ImageContainer}>
+            {image && <Image fill src={image} alt="게시글 이미지 미리보기" />}
+          </div>
         </div>
-      </div>
-      <div className={style.CardBottom}>
-        <p>{writer.nickname}</p>
-        <p>❤️{likeCount}</p>
-        <p>{formatDate(createdAt)}</p>
+        <div className={style.CardBottom}>
+          <div className={style.NameAndLikeCount}>
+            <p className={style.Name}>{writer.nickname}</p>
+            <p className={style.LikeCount}>❤️{likeCount}</p>
+          </div>
+          <p className={style.Date}>{formatDate(createdAt)}</p>
+        </div>
       </div>
     </div>
   );

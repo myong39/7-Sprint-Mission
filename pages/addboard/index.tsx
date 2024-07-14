623800cae14bd4df34bfb89e74bd0789321d1421
장아영 <jang-ayoung@jang-ayeong-ui-iMac.local>
2024-07-14 22:43:49 +0900

    Feat: 웹퍼블리싱 완료

--- pages/addboard/index.tsx
+++ pages/addboard/index.tsx
@@ -1,4 +1,8 @@
 import styles from "./styles.module.scss";
+import plusIcon from "@/assets/icons/ic_plus.png";
+import { ST } from "next/dist/shared/lib/utils";
+import Image from "next/image";
+
 export default function addboard() {
   return (
     <>
@@ -15,11 +19,34 @@ export default function addboard() {
           <div className={styles["form-box"]}>
             <div className={styles["title"]}>
               <label className={styles["label"]}>*제목</label>
-              <input className={styles['title-input-box']} type="text" placeholder="제목을 입력해주세요" required />
+              <input
+                className={styles["title-input-box"]}
+                type="text"
+                placeholder="제목을 입력해주세요"
+                required
+              />
             </div>
             <div className={styles["contents"]}>
               <label className={styles["label"]}>*내용</label>
-              <textarea className={styles['contents-input-box']} placeholder="내용을 입력해주세요" required />
+              <textarea
+                className={styles["contents-input-box"]}
+                placeholder="내용을 입력해주세요"
+                required
+              />
+            </div>
+            <div className={styles["image"]}>
+              <label className={styles["label"]}>이미지</label>
+              <div className={styles["img-register-box"]}>
+                <input className={styles["img-register"]} type="file" />
+                <div className={styles["img-register-button"]}>
+                  <Image
+                    className={styles["plus-icon"]}
+                    src={plusIcon}
+                    alt="파일추가버튼"
+                  />
+                  <p className={styles["register-button-text"]}>이미지 등록</p>
+                </div>
+              </div>
             </div>
           </div>
         </form>

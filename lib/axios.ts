919a013016f654b38a3dc87941c634793ef72e00
clyde-yoon <a1056719@gmail.com>
2024-07-15 19:16:01 +0900

    ✨모든게시물을 호출하는 api생성 및 호출함수 이름 변경

--- lib/axios.ts
+++ lib/axios.ts
@@ -5,16 +5,34 @@ const instance = axios.create({
   baseURL: 'https://panda-market-api.vercel.app',
 });
 
-export interface BestArticleResponse {
+export interface ArticleResponse {
   list: Article[];
 }
 
 export async function getBestArticle(pageSize: number): Promise<Article[]> {
-  const res = await instance.get<BestArticleResponse>('/articles', {
+  const res = await instance.get<ArticleResponse>('/articles', {
     params: {
       page: 1,
       pageSize: pageSize,
       orderBy: 'like',
+    },
+  });
+  const { list } = res.data;
+  return list;
+}
+
+export async function getArticle(
+  page: number,
+  pageSize: number,
+  orderBy: string,
+  keyword: string
+): Promise<Article[]> {
+  const res = await instance.get<ArticleResponse>('/articles', {
+    params: {
+      page: page,
+      pageSize: pageSize,
+      orderBy: orderBy,
+      keyword: keyword,
     },
   });
   const { list } = res.data;

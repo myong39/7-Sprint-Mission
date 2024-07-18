23b34df843382c232d0b9c8a9d5e2bb931bafc1a
Dangchan Kim <kdc5847@naver.com>
2024-07-18 23:07:15 +0900

    refactor: 홈으로돌아가기 버튼 추가

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,13 @@
 import styles from "@/styles/404.module.css";
+import Link from "next/link";
 
 export default function Custom404() {
-  return <div className={styles.container}>존재하지않는페이지입니다</div>;
+  return (
+    <div className={styles.container}>
+      <div>존재하지않는페이지입니다</div>
+      <Link href="/">
+        <button type="button">홈으로 돌아가기</button>
+      </Link>
+    </div>
+  );
 }

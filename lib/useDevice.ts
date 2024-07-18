8fa0c430688ec7aeccd3c06ed0cc5fe2430b59b7
Dangchan Kim <kdc5847@naver.com>
2024-07-19 00:12:38 +0900

    refactor: 디바이스 크기값 상수추가

--- lib/useDevice.ts
+++ lib/useDevice.ts
@@ -1,11 +1,21 @@
 import useWindowSize from "./useWindowSize";
 
+const PC_MIN_WIDTH = 1200;
+const TABLET_MIN_WIDTH = 768;
+const TABLET_MAX_WIDTH = 1199;
+const MOBILE_MIN_WIDTH = 375;
+const MOBILE_MAX_WIDTH = 767;
+
 export default function useDevice() {
   const windowSize = useWindowSize();
 
-  const isDesktop = windowSize.width >= 1200;
-  const isTablet = windowSize.width >= 768 && windowSize.width < 1200;
-  const isMobile = windowSize.width >= 375 && windowSize.width < 768;
+  const isDesktop = windowSize.width >= PC_MIN_WIDTH;
+  const isTablet =
+    windowSize.width >= TABLET_MIN_WIDTH &&
+    windowSize.width <= TABLET_MAX_WIDTH;
+  const isMobile =
+    windowSize.width >= MOBILE_MIN_WIDTH &&
+    windowSize.width <= MOBILE_MAX_WIDTH;
 
   return {
     isDesktop,

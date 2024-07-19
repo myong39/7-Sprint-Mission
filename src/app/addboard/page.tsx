5424ee38e14518fd59d8e53f66ab1e58271db973
JiinSeok <seokjiin1073@gmail.com>
2024-07-19 17:30:13 +0900

    feat: addboard 게시글 등록 함수 추가

--- src/app/addboard/page.tsx
+++ src/app/addboard/page.tsx
@@ -1,17 +1,35 @@
+'use client'
 
-export default async function AddBoard() {
+import axios from "@/lib/axios";
+import {createArticle} from "@/lib/requests";
+import {useState} from "react";
+
+export default function AddBoard() {
+  const [data, setData] = useState({ title: "", content: "", image: "" });
+
+  const handleChange = (e) => {
+    if (e.target.name === "image") {
+      setData("image", e.target.files[0]);
+    }
+    setData( { ...data, [e.target.name]: e.target.value });
+  }
+
+  const handleSubmit = (data) => {
+    createArticle(data);
+  }
 
   return (
     <>
-      <h1>게시글 쓰기</h1>
-      <form>
-        <input id="submit" type="submit" />
-        <label htmlFor="title">제목</label>
-        <input id="title" type="text" name="title" value="title" />
-        <label htmlFor="content">내용</label>
-        <textarea id="content" name="content" value="content"/>
+      <form onSubmit={handleSubmit}>
+        <button type="submit" disabled={!setData.title && !setData.content}>등록</button>
+        {/*<input type="hidden" name="accessToken" value="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwic2NvcGUiOiJhY2Nlc3MiLCJpYXQiOjE3MjEzNzU5MzMsImV4cCI6MTcyMTM3NzczMywiaXNzIjoic3AtcGFuZGEtbWFya2V0In0.lLKNlU8NqmVhvcpyUQ987jBhqAk67OUuMGPcdEoGVE0"/>*/}
+        {/*<input type="hidden" name="accessToken" value={localStorage.getItem("accessToken")}/>*/}
+        <label htmlFor="title">&#42;제목</label>
+        <input id="title" type="text" required name="title" placeholder="제목을 입력해 주세요." onChange={handleChange}/>
+        <label htmlFor="content">&#42;본문</label>
+        <textarea id="content" required name="content" placeholder="본문을 입력해 주세요." onChange={handleChange}/>
         <label htmlFor="image">이미지</label>
-        <input id="image" type="file" name="image" value="image"/>
+        <input id="image" type="file" accept="image/*" name="image" onChange={handleChange}/>
       </form>
     </>
   );

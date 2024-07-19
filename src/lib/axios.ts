f040108082baa420f8adc76872f183afa7da4918
JiinSeok <seokjiin1073@gmail.com>
2024-07-19 17:30:50 +0900

    chore: axios 임시 액세스토큰 등록

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://panda-market-api.vercel.app",
+  Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwic2NvcGUiOiJhY2Nlc3MiLCJpYXQiOjE3MjEzNzU5MzMsImV4cCI6MTcyMTM3NzczMywiaXNzIjoic3AtcGFuZGEtbWFya2V0In0.lLKNlU8NqmVhvcpyUQ987jBhqAk67OUuMGPcdEoGVE0",
 });
 
 export default instance;

3e082dd3804051ec602e93e340007a6fee32cea0
clyde-yoon <a1056719@gmail.com>
2024-07-20 23:20:00 +0900

    🐛 config 수정

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
   },
 };
 
-module.exports = nextConfig;
+module.exports = {
+  images: {
+    domains: [
+      'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+      'example.com',
+    ],
+  },
+};

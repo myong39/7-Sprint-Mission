ffed85b5bd4a830ea0d9618bafb5aec0dbb92d4c
장아영 <jang-ayoung@jang-ayeong-ui-iMac.local>
2024-07-20 23:24:14 +0900

    Feat: 이미지 등록

--- pages/addboard/index.tsx
+++ pages/addboard/index.tsx
@@ -2,8 +2,37 @@ import styles from "./styles.module.scss";
 import plusIcon from "@/assets/icons/ic_plus.png";
 import { ST } from "next/dist/shared/lib/utils";
 import Image from "next/image";
+import { useState, useRef, useEffect } from "react";
+import deleteIcon from "@/assets/icons/ic_delete.svg";
 
 export default function addboard() {
+  const [preview, setPreview] = useState();
+  const [value, setValue] = useState();
+  const fileInputRef = useRef(null);
+
+  const handleChange = (e) => {
+    const nextValue = e.target.files[0];
+    setValue(nextValue);
+  };
+
+  const handleImageClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleCancelClick = () => {
+    setPreview(null);
+    setValue(null);
+    fileInputRef.current.value = "";
+  };
+
+  useEffect(() => {
+    if (!value) return;
+
+    const nextPreview = URL.createObjectURL(value);
+    setPreview(nextPreview);
+    return () => URL.revokeObjectURL(nextPreview);
+  }, [value]);
+
   return (
     <>
       <div className={styles["page-container"]}>
@@ -36,16 +65,50 @@ export default function addboard() {
             </div>
             <div className={styles["image"]}>
               <label className={styles["label"]}>이미지</label>
-              <div className={styles["img-register-box"]}>
-                <input className={styles["img-register"]} type="file" />
-                <div className={styles["img-register-button"]}>
-                  <Image
-                    className={styles["plus-icon"]}
-                    src={plusIcon}
-                    alt="파일추가버튼"
+              <div className={styles["img-register-container"]}>
+                <div
+                  className={styles["img-register-box"]}
+                  onClick={handleImageClick}
+                >
+                  <input
+                    ref={fileInputRef}
+                    className={styles["img-register"]}
+                    type="file"
+                    onChange={handleChange}
                   />
-                  <p className={styles["register-button-text"]}>이미지 등록</p>
+                  <div className={styles["img-register-button"]}>
+                    <Image
+                      className={styles["plus-icon"]}
+                      src={plusIcon}
+                      alt="파일추가버튼"
+                    />
+                    <p className={styles["register-button-text"]}>
+                      이미지 등록
+                    </p>
+                  </div>
                 </div>
+                {preview && (
+                  <div className={styles["preview-image-box"]}>
+                    <Image
+                      className={styles["preview-image"]}
+                      src={preview}
+                      alt="이미지 미리보기"
+                      width="282"
+                      height="282"
+                    />
+                    <button
+                      className={styles["delete-preview-image"]}
+                      onClick={handleCancelClick}
+                    >
+                      <Image
+                        src={deleteIcon}
+                        alt="미리보기 삭제"
+                        width="8"
+                        height="8"
+                      />
+                    </button>
+                  </div>
+                )}
               </div>
             </div>
           </div>

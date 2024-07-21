e0a52a7e78a530319dcf6b3d44784ea39b669edb
장아영 <jang-ayoung@jang-ayeong-ui-iMac.local>
2024-07-22 00:23:22 +0900

    Feat:버튼 비활성화 기능 추가

--- pages/addboard/index.tsx
+++ pages/addboard/index.tsx
@@ -5,10 +5,21 @@ import Image from "next/image";
 import { useState, useRef, useEffect } from "react";
 import deleteIcon from "@/assets/icons/ic_delete.svg";
 
-export default function addboard() {
+export default function AddBoard() {
   const [preview, setPreview] = useState();
   const [value, setValue] = useState();
   const fileInputRef = useRef(null);
+  const [formState, setFormState] = useState({
+    title: "",
+    contents: "",
+  });
+
+  const handleInput = (e) => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
@@ -33,87 +44,94 @@ export default function addboard() {
     return () => URL.revokeObjectURL(nextPreview);
   }, [value]);
 
+  const isFormValid = formState.title && formState.contents;
+
   return (
-    <>
-      <div className={styles["page-container"]}>
-        <div className={styles["title-and-button"]}>
-          <p className={styles["form-title"]}>게시글 쓰기</p>
-          <input
-            className={styles["register-button"]}
-            type="button"
-            value="등록"
-          />
-        </div>
-        <form>
-          <div className={styles["form-box"]}>
-            <div className={styles["title"]}>
-              <label className={styles["label"]}>*제목</label>
-              <input
-                className={styles["title-input-box"]}
-                type="text"
-                placeholder="제목을 입력해주세요"
-                required
-              />
-            </div>
-            <div className={styles["contents"]}>
-              <label className={styles["label"]}>*내용</label>
-              <textarea
-                className={styles["contents-input-box"]}
-                placeholder="내용을 입력해주세요"
-                required
-              />
-            </div>
-            <div className={styles["image"]}>
-              <label className={styles["label"]}>이미지</label>
-              <div className={styles["img-register-container"]}>
-                <div
-                  cflassName={styles["img-register-box"]}
-                  onClick={handleImageClick}
-                >
-                  <input
-                    ref={fileInputRef}
-                    className={styles["img-register"]}
-                    type="file"
-                    onChange={handleChange}
+    <div className={styles["page-container"]}>
+      <div className={styles["title-and-button"]}>
+        <p className={styles["form-title"]}>게시글 쓰기</p>
+        <button
+          className={`${styles["register-button"]} ${
+            isFormValid ? styles["register-button-active"] : ""
+          }`}
+          disabled={!isFormValid}
+          type="submit"
+        >
+          등록
+        </button>
+      </div>
+      <form>
+        <div className={styles["form-box"]}>
+          <div className={styles["title"]}>
+            <label className={styles["label"]}>*제목</label>
+            <input
+              className={styles["title-input-box"]}
+              type="text"
+              name="title"
+              onChange={handleInput}
+              placeholder="제목을 입력해주세요"
+              required
+            />
+          </div>
+          <div className={styles["contents"]}>
+            <label className={styles["label"]}>*내용</label>
+            <textarea
+              className={styles["contents-input-box"]}
+              placeholder="내용을 입력해주세요"
+              name="contents"
+              onChange={handleInput}
+              required
+            />
+          </div>
+          <div className={styles["image"]}>
+            <label className={styles["label"]}>이미지</label>
+            <div className={styles["img-register-container"]}>
+              <div
+                className={styles["img-register-box"]}
+                onClick={handleImageClick}
+              >
+                <input
+                  ref={fileInputRef}
+                  className={styles["img-register"]}
+                  type="file"
+                  name="image"
+                  onChange={handleChange}
+                />
+                <div className={styles["img-register-button"]}>
+                  <Image
+                    className={styles["plus-icon"]}
+                    src={plusIcon}
+                    alt="파일추가버튼"
                   />
-                  <div className={styles["img-register-button"]}>
-                    <Image
-                      className={styles["plus-icon"]}
-                      src={plusIcon}
-                      alt="파일추가버튼"
-                    />
-                    <p className={styles["register-button-text"]}>
-                      이미지 등록
-                    </p>
-                  </div>
+                  <p className={styles["register-button-text"]}>이미지 등록</p>
                 </div>
-                {preview && (
-                  <div className={styles["preview-image-box"]}>
-                    <Image
-                      className={styles["preview-image"]}
-                      src={preview}
-                      alt="이미지 미리보기"
-                      width="282"
-                      height="282"
-                    />
-                    <button
-                      className={styles["delete-preview-image"]}
-                      onClick={handleCancelClick}
-                    >
-                      <Image
-                        src={deleteIcon}
-                        alt="미리보기 삭제"
-                        width="8"
-                        height="8"
-                      />
-                    </button>
-                  </div>
-                )}
               </div>
+              {preview && (
+                <div className={styles["preview-image-box"]}>
+                  <Image
+                    className={styles["preview-image"]}
+                    src={preview}
+                    alt="이미지 미리보기"
+                    width="282"
+                    height="282"
+                  />
+                  <button
+                    className={styles["delete-preview-image"]}
+                    onClick={handleCancelClick}
+                  >
+                    <Image
+                      src={deleteIcon}
+                      alt="미리보기 삭제"
+                      width="8"
+                      height="8"
+                    />
+                  </button>
+                </div>
+              )}
             </div>
           </div>
-        </form>
-      </div>
-    </>
+        </div>
+      </form>
+    </div>
   );
 }

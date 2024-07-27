4b72382005c18f64dff9e5f40f23eb6c7bd7a3af
Dangchan Kim <kdc5847@naver.com>
2024-07-28 00:35:22 +0900

    feat: 404에러 페이지 수정

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -4,7 +4,14 @@ import "./NotFound.css";
 const NotFound = () => {
   return (
     <div className="notFound-container">
-      <div>페이지를 찾을 수 없습니다.</div>
+      <h2>404 ERROR</h2>
+      <div>
+        페이지를 찾을 수 없습니다.
+        <br />
+        존재하지 않는 주소를 입력하셨거나,
+        <br />
+        요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다.
+      </div>
       <Link to="/">
         <button type="button">홈으로 이동</button>
       </Link>

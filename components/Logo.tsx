a04a8a6da54c88bf0f2d659f3c42f1cbc8f70092
minkyung <mxkxx@naver.com>
2024-07-28 19:51:05 +0900

    refactor: 로고 이미지 수정

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,15 +2,9 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 function Logo() {
-  // 로고 바뀌는거..
   return (
     <Link href='/'>
-      <Image
-        src='/images/image-logo-img.svg'
-        width={153}
-        height={51}
-        alt='logo'
-      />
+      <Image src='/images/image-logo-img.svg' fill alt='logo' />
     </Link>
   );
 }

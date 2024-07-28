e5d900390501298b449f695f522a1018160bb4ad
minkyung <mxkxx@naver.com>
2024-07-28 19:52:52 +0900

    feat: axios instance interceptor 추가

--- lib/axios.tsx
+++ lib/axios.tsx
@@ -1,7 +1,25 @@
-import axios from 'axios';
+import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
+
+// 이게 맞는지 잘 모르겠습니다 ㅠ
+instance.interceptors.response.use(
+  (res) => res,
+  async (error: AxiosError) => {
+    const originalRequest = error.config!;
+    if (error.response?.status === 401) {
+      try {
+        const refToken = localStorage.getItem('refreshToken');
+        await instance.post('/auth/refresh-token', refToken);
+        return instance(originalRequest);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
